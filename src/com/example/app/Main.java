63782903d92f9f49970024dbb7886b3f20b2a2d0
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-04-10 00:17:40 +0200

    read file structure added

--- src/com/example/app/Main.java
+++ src/com/example/app/Main.java
@@ -11,6 +11,13 @@ public class Main {
         try{
             fis = new FileInputStream("File.txt");
 
+            int c;
+
+            while((c = fis.read()) != -1) {
+                System.out.print((char)c);
+            }
+
+
         }finally {
          if(fis != null)
             fis.close();
