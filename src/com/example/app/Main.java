0244f9a9b1c0ce1a1fbcd0940cdc31e079c0cc1b
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-04-10 00:14:24 +0200

    project update

--- src/com/example/app/Main.java
+++ src/com/example/app/Main.java
@@ -1,8 +1,20 @@
 package com.example.app;
 
+import java.io.FileInputStream;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+
+        FileInputStream fis = null;
+
+        try{
+            fis = new FileInputStream("File.txt");
+
+        }finally {
+         if(fis != null)
+            fis.close();
+        }
+
     }
 }
