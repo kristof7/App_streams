0eb5ffbc3c25cd0405db1d29cefdb4fa2d8d482c
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-04-10 00:20:07 +0200

    save to file structure added

--- src/com/example/app/Main.java
+++ src/com/example/app/Main.java
@@ -1,20 +1,25 @@
 package com.example.app;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
         FileInputStream fis = null;
+        FileOutputStream fos = null;
 
         try{
             fis = new FileInputStream("File.txt");
+
+            fos = new FileOutputStream("fosFile.txt");
 
             int c;
 
             while((c = fis.read()) != -1) {
                 System.out.print((char)c);
+                fos.write(c);
             }
 
 
