5e364061d044aebf6c5e636a3d4be55fb15ceffa
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-04-10 19:57:21 +0200

    sample BuferredWriter construction added

--- src/com/example/app/brbw/Main.java
+++ src/com/example/app/brbw/Main.java
@@ -7,19 +7,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = null;
+        BufferedWriter bw = null;
 
         try{
             br = new BufferedReader(new FileReader("file.txt"));
+            bw = new BufferedWriter(new FileWriter("bwFile.txt"));
             String s;
 
             while ((s = br.readLine()) != null) {
                 System.out.println(s);
+                bw.write(s);
             }
 
         }finally {
          if(br != null)
             br.close();
-
+         if(bw != null)
+             bw.close();
         }
     }
 }
